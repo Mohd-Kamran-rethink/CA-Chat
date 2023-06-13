9163a50a78ed330470b7e9afcd09eeeddcef323f
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-06-13 15:11:14 +0530

    changes

---
+++ resources/views/Customdashboard.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CA-Chat</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body style="background: rgb(234, 111, 11)">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">CA-Chat</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('chatify') }}">Chats</a>
+                    </li>
+                    <li class="nav-item">
+                    </li>
+
+                </ul>
+                <ul class="navbar-nav ">
+                    <form method="POST" action="{{ route('logout') }}" class="inline">
+                        @csrf
+
+                        <button type="submit" class="" style="background: none">
+                            {{ __('Log Out') }}
+                        </button>
+                    </form>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <main role="main" class="container text-center absolute"
+        style="position: absolute;top: 50%;left: 50%;transform: translate(-50% ,-50%)">
+
+        <div class="starter-template">
+            <h1>Cric-Adda ChatApp</h1>
+            <p class="lead font-weight-bold" style="font-weight: 600">The site is under development phase, you can only
+                test it for now!</p>
+            <a href="{{ url('chatify') }}" class="btn btn-dark">Start Chat</a>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,80 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CA-Chat | Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+<body style="background: rgb(234, 111, 11)">
+    <div class="container"  style="position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);">
+        <div class="row justify-content-center" style="">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                       <h4 class="text-center">Crick Adda | ChatApp </h4> 
+                    </div>
+
+                    <div class="card-body">
+                        <x-jet-validation-errors class="mb-4" />
+
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="form-group">
+                                <x-jet-label for="email" value="{{ __('Email') }}" />
+                                <x-jet-input id="email" class="form-control" type="email" name="email"
+                                    :value="old('email')" required autofocus />
+                            </div>
+
+                            <div class="form-group mt-4">
+                                <x-jet-label for="password" value="{{ __('Password') }}" />
+                                <x-jet-input id="password" class="form-control" type="password" name="password"
+                                    required autocomplete="current-password" />
+                            </div>
+
+                            <div class="form-group form-check mt-4">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember_me">
+                                <label class="form-check-label" for="remember_me">
+                                    {{ __('Remember me') }}
+                                </label>
+                            </div>
+
+                            {{-- <div class="form-group mt-4">
+                                @if (Route::has('password.request'))
+                                    <a class="underline text-sm text-gray-600"
+                                        href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                @endif
+                            </div> --}}
+
+                            <div class="form-group mt-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Log in') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        @endif
+        </div>
+    </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</html>
