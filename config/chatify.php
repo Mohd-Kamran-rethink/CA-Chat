34204a68e69c21449acd49e2cb683a442d963bc3
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-06-14 19:12:26 +0530

    chhdf

--- config/chatify.php
+++ config/chatify.php
@@ -14,7 +14,7 @@ return [
     | files and derived images by default.
     |-------------------------------------
     */
-    'storage_disk_name' => env('CHATIFY_STORAGE_DISK', 'public_html'),
+    'storage_disk_name' => env('CHATIFY_STORAGE_DISK', 'public'),
 
     /*
     |-------------------------------------
